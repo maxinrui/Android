e06a061942773658a89b129de181633557de9115
Xinrui Ma <xma@C07N80JSDY3H.local>
2015-04-15 16:46:36 -0400

    Finish Fun facts

--- FunFacts/app/src/main/java/me/xinruima/www/funfacts/FunFactsActivity.java
+++ FunFacts/app/src/main/java/me/xinruima/www/funfacts/FunFactsActivity.java
@@ -4,36 +4,47 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 
 public class FunFactsActivity extends ActionBarActivity {
+
+    private FactBook mFactBook = new FactBook();
+    private ColorPanel mColorPanel = new ColorPanel();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        // declare our view variables
+        final TextView factLabel = (TextView)findViewById(R.id.factTextView);
+        final Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        final RelativeLayout mLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Update the fact label with new fact
+                String fact = "";
+
+                fact = mFactBook.getFact();
+
+                factLabel.setText(fact);
+                mLayout.setBackgroundColor(mColorPanel.getColor());
+                showFactButton.setTextColor(mColorPanel.getColor());
+            }
+        };
+        showFactButton.setOnClickListener(listener);
+
+        Toast.makeText(this,"Xinrui is really Handsome!",Toast.LENGTH_LONG).show();
+
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_fun_facts, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
