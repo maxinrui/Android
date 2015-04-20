e1c0fd4af4c0305a51707e8280a999d04ab05d53
xinruima <maxinrui1027@gmail.com>
2015-04-19 23:18:09 -0400

    add new stuff

--- Story/app/src/main/java/me/xinruima/www/story/MainActivity.java
+++ Story/app/src/main/java/me/xinruima/www/story/MainActivity.java
@@ -1,12 +1,13 @@
 package me.xinruima.www.story;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
