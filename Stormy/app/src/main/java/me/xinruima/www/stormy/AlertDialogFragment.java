94adc9d352332022421b2cf8f31e9ee3841ca415
Xinrui Ma <xma@C07N80JSDY3H.local>
2015-05-08 15:05:02 -0400

    init

---
+++ Stormy/app/src/main/java/me/xinruima/www/stormy/AlertDialogFragment.java
@@ -0,0 +1,7 @@
+package me.xinruima.www.stormy;
+
+/**
+ * Created by xma on 5/8/15.
+ */
+public class AlertDialogFragment {
+}
---
+++ Stormy/app/src/main/java/me/xinruima/www/stormy/MainActivity.java
@@ -0,0 +1,60 @@
+package me.xinruima.www.stormy;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String apiKey = "c15734f894e7201106402ff9d7ac0fb2";
+        double latitude = 37.8267;
+        double longitude = -122.423;
+        String forecastUrl = "https://api.forecast.io/forecast/" + apiKey + "/" + latitude + "," + longitude;
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(forecastUrl)
+                .build();
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                try {
+                    if (response.isSuccessful()) {
+                        Log.v(TAG, response.body().string());
+                    } else {
+                        Log.v(TAG, response.body().string());
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "Exception caught", e);
+                }
+            }
+        });
+    }
+
+    private void alertUserAboutError() {
+    }
+}
